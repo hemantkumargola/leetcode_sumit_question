1c14e195d7ddd8d50344eca3f1dac5217c6db562
Hemant kumar gola <149658773+hemantkumargola@users.noreply.github.com>
2026-07-28 10:04:17 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (11.49%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,32 +1,57 @@
+// class Solution {
+
+//     public double myPow(double x, int n) {
+
+//         if (n >= 0) {
+//             return qpow(x, (long) n);
+//         } else {
+//             return 1 / qpow(x, -(long) n);
+//         }
+//     }
+
+//     private double qpow(double a, long n) {
+
+//         double ans = 1;
+
+//         while (n > 0) {
+
+//             if ((n & 1) == 1) {
+//                 ans = ans * a;
+//             }
+
+//             a = a * a;
+//             n = n / 2;      // or n >>= 1;
+//         }
+
+//         return ans;
+//     }
+// }
 class Solution {
 
     public double myPow(double x, int n) {
 
-        if (n >= 0) {
-            return qpow(x, (long) n);
-        } else {
-            return 1 / qpow(x, -(long) n);
-        }
-    }
+        long power = n;
 
-    private double qpow(double a, long n) {
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
 
         double ans = 1;
 
-        while (n > 0) {
+        while (power > 0) {
 
-            if ((n & 1) == 1) {
-                ans = ans * a;
+            if (power % 2 == 1) {
+                ans *= x;
             }
 
-            a = a * a;
-            n = n / 2;      // or n >>= 1;
+            x *= x;
+            power /= 2;
         }
 
         return ans;
     }
 }
-
 
 // | Complexity           | Value        |
 // | -------------------- | ------------ |
