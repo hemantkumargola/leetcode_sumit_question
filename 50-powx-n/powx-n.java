c7d5d626b55a400fe89e919b83755a81e3cd507d
Hemant kumar gola <149658773+hemantkumargola@users.noreply.github.com>
2026-07-29 09:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (65.12%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,32 +1,24 @@
 class Solution {
-
     public double myPow(double x, int n) {
-
-        long power = n;
-
+       long power = n;
         if (power < 0) {
             x = 1 / x;
             power = -power;
         }
-
-        return power(x, power);
+         return power(x, power);
     }
 
     private double power(double x, long n) {
-
-        // Base Case
-        if (n == 0) {
+        //basec case
+      if (n == 0) {
             return 1;
         }
-
-        // Recursive Call
+      // Recursive Call
         double half = power(x, n / 2);
-
-        // Even Power
+       // Even Power
         if (n % 2 == 0) {
             return half * half;
         }
-
         // Odd Power
         return x * half * half;
     }
