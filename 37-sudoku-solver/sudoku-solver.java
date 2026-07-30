a4dad0f547f0761561c043ac75411d67a98b1f47
Hemant kumar gola <149658773+hemantkumargola@users.noreply.github.com>
2026-07-30 12:08:18 +0530

    Time: 384 ms (12.13%) | Memory: 42.9 MB (35.44%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    for (char ch = '1'; ch <= '9'; ch++) {
+
+                        if (isValid(board, row, col, ch)) {
+
+                            board[row][col] = ch;
+
+                            if (solve(board))
+                                return true;
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char ch) {
+
+        for (int i = 0; i < 9; i++) {
+
+            // Row check
+            if (board[row][i] == ch)
+                return false;
+
+            // Column check
+            if (board[i][col] == ch)
+                return false;
+
+            // 3x3 Box check
+            int newRow = 3 * (row / 3) + i / 3;
+            int newCol = 3 * (col / 3) + i % 3;
+
+            if (board[newRow][newCol] == ch)
+                return false;
+        }
+
+        return true;
+    }
+}
