e3da0408a6dab10fa9c59ccd56322801e07e003f
Hemant kumar gola <149658773+hemantkumargola@users.noreply.github.com>
2026-08-02 09:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (16.66%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -3,6 +3,8 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
+
+        // fast.next!=nul&&fast !=null  ye condition wrong hai 
         while(fast !=null && fast.next !=null){
             slow = slow.next;
             fast = fast.next.next;
