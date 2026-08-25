bc764b798198eadadedcbbe09c959ff1431b38f8
Hemant kumar gola <149658773+hemantkumargola@users.noreply.github.com>
2026-08-25 08:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (92.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -17,32 +17,40 @@
 
 
 
-// solved memoazation 
+// // solved memoazation 
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for(int i = 0;i<m;i++){
+//             for(int j = 0;j<n;j++){
+//                 dp[i][j] = -1;
+//             }
+//         }
+//          return  path(0,0,m,n,dp);
+//     }
+//          public int path(int row,int col , int m , int   n ,int[][] dp){
+//         if(row>=m || col>=n) return 0;
+//         if(row == m-1 || col == n-1)  return 1;
+//         if(dp[row][col] != -1) return dp[row][col];
+//         int rightway = path(row,col+1,m,n,dp);
+//         int letfway = path(row+1,col , m ,n ,dp);
+//         return dp[row][col] = rightway + letfway;
+//     }
+// }
+
+
+
+// tabular methods 
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                dp[i][j] = -1;
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(i==0 ||j == 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i][j-1] +dp[i-1][j];
+
             }
         }
-         return  path(0,0,m,n,dp);
+        return dp[m-1][n-1];
     }
-         public int path(int row,int col , int m , int   n ,int[][] dp){
-        if(row>=m || col>=n) return 0;
-        if(row == m-1 || col == n-1)  return 1;
-        if(dp[row][col] != -1) return dp[row][col];
-        int rightway = path(row,col+1,m,n,dp);
-        int letfway = path(row+1,col , m ,n ,dp);
-        return dp[row][col] = rightway + letfway;
-    }
-}
-
-
-
-// // tabular methods 
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-        
-//     }
-// }   
+}   
